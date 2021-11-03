4379c87cafb3bcdf7e48bc92616c171eedaf5284
Luiza Prata Soldi Passos <luiza.psp@gmail.com>
2021-11-03 12:02:02 -0300

    ajustes rn

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, PixelRatio } from "react-native";
+import * as variables from "./style-dictionary-dist/variables";
 
-// custom components for demo
-import ComponentWithStyledComponents from './components/with-styled-components/ComponentWithStyledComponents';
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        
-        <main>
-          <ComponentWithStyledComponents />
-        </main>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Testing</Text>
+      <Text style={styles.p}>All done!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: variables.brandColorPrimary1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: PixelRatio.getFontScale() * variables.fontSizeLg.number,
+    color: variables.brandColorPrimary4,
+  },
+  p: {
+    fontSize: PixelRatio.getFontScale() * variables.fontSizeSm.number,
+    color: variables.neutralColor4,
+  },
+});
